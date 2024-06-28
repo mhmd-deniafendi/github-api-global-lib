4981bc6a5ef0f7bf97fa4eace328deb18568258c
Muhamad Deni Afendi <muhamaddeniafendi@prudential.com.sg>
2024-06-28 20:15:24 +0700

    adding function

--- vars/greetingHello.groovy
+++ vars/greetingHello.groovy
@@ -1,3 +1,11 @@
 def world(String name) {
     echo("Hello ${name}")
 }
+
+def os(String OS_VERSION) {
+    echo("OS: ${OS_VERSION}")
+}
+
+def addapp(String APP) {
+    echo("Applicatoin: ${APP}")
+}
